0179fb9a2b0cdf11be462d857dede71c6e90108d
msun4100 <msun4100@gmail.com>
2020-05-25 19:44:43 +0900

    Classes on TS

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "KMS",
-  age: 22,
-  gender: "male",
-};
+const person = new Human("Lynn", 18, "female");
+
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
